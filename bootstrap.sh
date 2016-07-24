4dcba7f64ad53154661abf692ebc1d7a3c73691e
Robert Claypool <robert.g.claypool+git@gmail.com>
2016-07-24 14:58:25 -0500

    Update git config if git is found

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,10 @@ ln -sf $PWD/system/.env $HOME/.env
 ln -sf $PWD/system/.alias $HOME/.alias
 ln -sf $PWD/.tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/.gitignore $HOME/.gitignore
+
+if command -v git >/dev/null 2>&1; then
+    git config --global core.excludesfile ~/.gitignore
+else
+    echo "error: cannot find git. gitconfig was not updated"
+    exit 1
+fi
