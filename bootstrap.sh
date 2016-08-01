96c0fb4ec0f1efa6847defc41d1a5d7a12080072
Robert Claypool <robert.g.claypool+git@gmail.com>
2016-08-01 13:01:05 -0500

    Add psql configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 ln -sf $PWD/system/.env $HOME/.env
 ln -sf $PWD/system/.alias $HOME/.alias
+ln -sf $PWD/.psqlrc $HOME/.psqlrc
 ln -sf $PWD/.tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/.gitignore $HOME/.gitignore
 
