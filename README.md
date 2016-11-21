498bc0ddbab73ba8f6ca680a41404b7452c087bd
Robert Claypool <robert.g.claypool+git@gmail.com>
2016-11-21 14:16:14 -0600

    Add 256 colors test + tmux statusbar settings

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dotfiles
 My dotfiles, except [.vim](https://github.com/robert-claypool/.vim).
+
+1. Run `bootstrap.sh`.
+2. ?
+3. PROFIT
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
+echo "Setting symbolic links to various configs..."
 ln -sf $PWD/system/.env $HOME/.env
 ln -sf $PWD/system/.alias $HOME/.alias
 ln -sf $PWD/.psqlrc $HOME/.psqlrc
 ln -sf $PWD/.tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/.gitignore $HOME/.gitignore
+echo "Done."
 
 if command -v git >/dev/null 2>&1; then
     git config --global core.excludesfile ~/.gitignore
 else
-    echo "error: cannot find git. gitconfig was not updated"
+    echo "Error: Cannot find Git. '.gitconfig' was not updated."
     exit 1
+fi
+
+if command -v python >/dev/null 2>&1; then
+    python $PWD/util/terminalcolors.py
+    echo "If you don't see 256 unique colors, then try"
+    echo "adding 'export TERM=\"xterm-256color\"' to the end of '.bashrc'"
+    echo "and open a new shell. (This works in Debian, I don't know about"
+    echo "other distros.)"
+    exit 0
+else
+    echo "Warning: Cannot find Python. Unable to run 'util/terminalcolors.py'."
+    exit 0
 fi
