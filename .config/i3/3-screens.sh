0e95851c818d73801b981dc17c5a165e27b04e9d
Robert Claypool <robert.g.claypool+git@gmail.com>
2018-04-20 12:11:01 -0500

    Add 3 screens config

---
+++ .config/i3/3-screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --off --output DP-6 --off --output DP-5 --off --output DP-4 --mode 3200x1800 --pos 0x592 --rotate normal --output DP-3 --off --output DP-2 --mode 1920x1080 --pos 3200x0 --rotate normal --output DP-1 --off --output DP-0 --mode 1920x1080 --pos 5120x0 --rotate normal
