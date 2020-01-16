64184b35bd1a53d6676a98f61bab6dffd04b36fd
Robert Claypool <robert.g.claypool@gmail.com>
2020-01-15 20:34:33 -0600

    Now HiDPI is better for Dell Precision 7520

--- bootstrap.sh
+++ bootstrap.sh
@@ -56,8 +56,7 @@ if command -v python >/dev/null 2>&1; then
     python $PWD/util/terminalcolors.py
     echo "If you don't see 256 unique colors, then try"
     echo "adding 'export TERM=\"xterm-256color\"' to the end of '.bashrc'"
-    echo "and open a new shell. (This works in Debian, I don't know about"
-    echo "other distros.)"
+    echo "and open a new shell. (This works in Debian, Arch, and Manjaro.)"
 else
     echo "Warning: Cannot find Python. Unable to run 'util/terminalcolors.py'."
     exit 0
